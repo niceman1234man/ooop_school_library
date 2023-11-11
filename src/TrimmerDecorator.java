5997424a95d2b2331dbf8129996d905b7caa9b35
niceman1234man <yihunietarekegn18@gmail.com>
2023-11-11 06:13:53 -0200

    decoration of name of person

---
+++ src/TrimmerDecorator.java
@@ -0,0 +1,13 @@
+public class TrimmerDecorator extends Decorator{
+
+    public TrimmerDecorator(Nameable nameable) {
+        super(nameable);
+    }
+    public String correctName() {
+        String name = nameable.correctName();
+        if (name.length() > 10) {
+            return name.substring(0, 10);//shorten the name whose is length greater than 10
+        }
+        return name;
+    }
+}
