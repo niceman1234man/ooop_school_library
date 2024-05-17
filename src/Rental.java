fab5ba374b7a42fa56c2349026781cad4ae9c102
Yihunie Tarekegn <136583776+niceman1234man@users.noreply.github.com>
2024-05-17 21:33:36 +0300

    Update Rental.java

--- src/Rental.java
+++ src/Rental.java
@@ -2,25 +2,19 @@ public class Rental {
     private String date;
     private Book book;
     private Person person;
-
     public Rental(String date, Book book, Person person) {
         this.date = date;
         this.book = book;
         this.person = person;
     }
-
     public String getDate() {
         return date;
     }
-
     public void setDate(String date) {
-
         this.date = date;
     }
 
     public Book getBook() {
         return book;
     }
-
-
 }
