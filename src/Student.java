3db2170e82428adac1889a0920617319bc80ea1a
Yihunie Tarekegn <136583776+niceman1234man@users.noreply.github.com>
2024-05-17 21:34:01 +0300

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,12 @@
 
 public class Student extends Person {
     String classroom;
-
     public Student(int id,String name, int age, String classroom) {
         super(id,name, age);
          this.classroom = classroom;
     }
-
     public void setClassroom(Classroom classroom) {
         this.classroom = this.classroom;
         classroom.addStudent(this);
-
     }
-
 }
