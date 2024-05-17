9f8d6ab8099e975fb37dc82cf25b89de6dc01018
Yihunie Tarekegn <136583776+niceman1234man@users.noreply.github.com>
2024-05-17 21:34:44 +0300

    Update Teacher.java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,12 +1,8 @@
 
-
 public class Teacher extends Person {
      private String specialization;
-
     public Teacher(int id,String name, int age, String specialization) {
         super(id,name, age);
          this.specialization = specialization;
     }
-
-
 }
